f4eaa0b39deb700a7c96063a3c5dfb6608ea4f86
dannydktr <93729067+dannydktr@users.noreply.github.com>
2023-02-12 18:18:04 -0600

    finished mostly

---
+++ script.js
@@ -0,0 +1,67 @@
+const buttonContainer = document.querySelectorAll(".button-container");
+const totalDisplay = document.querySelector(".total-cost");
+const orderButton = document.querySelector(".order-button");
+const clearButton = document.querySelector(".clear-button");
+
+
+
+let total = 0;
+const menu_items = ["Mac & Cheese", "Pasta", "Taco", "Enchilada"];
+
+buttonContainer.forEach(function (set) {
+    let number = 0;
+    const factor = parseInt(set.dataset.factor);
+    const incrementButton = set.querySelector(".increment");
+    const decrementButton = set.querySelector(".decrement");
+    const numberDisplay = set.querySelector(".number");
+
+    incrementButton.addEventListener("click", function () {
+        number++;
+        total += factor;
+        numberDisplay.textContent = number;
+        totalDisplay.textContent = total;
+    });
+
+    decrementButton.addEventListener("click", function () {
+        if (number > 0) {
+            number--;
+            total -= factor;
+        }
+        numberDisplay.textContent = number;
+        totalDisplay.textContent = total;
+    });
+
+    clearButton.addEventListener("click", function () {
+
+        buttonContainer.forEach(function (set) {
+            const numberDisplay = set.querySelector(".number");
+            number = 0;
+            numberDisplay.textContent = 0;
+        });
+        total = 0;
+        totalDisplay.textContent = 0
+    });
+
+});
+
+orderButton.addEventListener("click", function () {
+    let order_message = "Order Placed!\n";
+    let index = 0;
+    buttonContainer.forEach(function (set) {
+        const numberDisplay = set.querySelector(".number");
+        order_message += numberDisplay.textContent + " " + menu_items[index] + "\n";
+        index++;
+        number = 0;
+        numberDisplay.textContent = 0;
+    });
+    total = 0;
+    totalDisplay.textContent = 0;
+    alert(order_message);
+    buttonContainer.forEach(function (set) {
+        clearButton.click();
+    });
+});
+
+
+
+
